9ebc3ae5e0832674889035b0b1eb9f9d93e0a716
OmDwivedi1237 <26dwivediom@gmail.com>
2024-09-27 02:13:46 -0700

    changed animation delay times

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,33 @@
 
 import { NavbarDemo } from "@/components/ui/Nav";
 import { motion } from "framer-motion"
+import "./globals.css";
 
 export default function Home() {
   return (
     <div className="w-full h-screen">
-      <NavbarDemo/>
+      <motion.div
+        initial={{opacity:0, y: 20}}
+        animate={{opacity:1, y: 0}}
+        transition={{type:"string", duration:2, delay:2}}
+      >
+        <NavbarDemo/>
+      </motion.div>
       <div className="flex items-center justify-center h-full">
         <div className="text-4xl font-bold text-black dark:text-white w-1/2 grid grid-cols-2 grid-rows-1 gap-0 h-16">
           <motion.div
-          initial={{opacity:0, y: -20}}
-          animate={{opacity:1, y: 0}}
-          transition={{type:"string", duration:1}}
+            initial={{opacity:0, y: -20}}
+            animate={{opacity:1, y: 0}}
+            transition={{type:"string", duration:1}}
           >
-            <span className="flex items-start justify-end">Cyber</span>
+            <span className="flex items-start justify-end garet-book-font">Cyber</span>
           </motion.div>
           <motion.div
-          initial={{opacity:0, y: 20}}
-          animate={{opacity:1, y: 0}}
-          transition={{type:"string", duration:1, delay:1}}
+            initial={{opacity:0, y: 20}}
+            animate={{opacity:1, y: 0}}
+            transition={{type:"string", duration:0.8, delay:0.8}}
           >
-            <span className="flex justify-start items-end">nexus</span>
+            <span className="flex justify-start items-end garet-book-font">Nexus</span>
           </motion.div>
         </div>
       </div>
